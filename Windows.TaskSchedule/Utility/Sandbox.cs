9128ce8e1bd08e566d5828cd16d053d33748b4fe
Erik Tse <eriketse@gmail.com>
2017-01-05 15:09:31 +0800

    添加沙盒模式

---
+++ Windows.TaskSchedule/Utility/Sandbox.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+
+namespace Windows.TaskSchedule.Utility
+{
+    public class Sandbox : MarshalByRefObject
+    {
+        private static AppDomain _domain = null;
+
+        public static Sandbox Create(string domainName = null)
+        {
+            //方法一： 权限集和配置从创建的AppDomain继承
+            domainName = domainName ?? "Sandbox" + new Random().Next(1000);
+            _domain = AppDomain.CreateDomain(domainName, null, null);
+            Sandbox box = _domain.CreateInstanceAndUnwrap(Assembly.GetAssembly(typeof(Sandbox)).FullName, typeof(Sandbox).ToString()) as Sandbox;
+            return box;
+
+            //方法二：自定义权限集和配置
+            //domainName = domainName ?? "Sandbox" + new Random().Next(1000);
+
+            //var setup = new AppDomainSetup()
+            //{
+            //    ApplicationBase = AppDomain.CurrentDomain.BaseDirectory,
+            //    ApplicationName = domainName,
+            //    DisallowBindingRedirects = true,
+            //    DisallowCodeDownload = true,
+            //    DisallowPublisherPolicy = true
+            //};
+
+            //var permissions = new PermissionSet(PermissionState.None);
+            //permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess));
+            //permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+
+            ////var fullTrustAssembly = typeof(Sandbox).Assembly.Evidence.GetHostEvidence<StrongName>();
+            ////_domain = AppDomain.CreateDomain(domainName, null, setup, permissions, fullTrustAssembly);
+            //_domain = AppDomain.CreateDomain(domainName, null, setup, permissions);
+
+            //Sandbox box =
+            //    (Sandbox)
+            //        Activator.CreateInstanceFrom(_domain, typeof(Sandbox).Assembly.ManifestModule.FullyQualifiedName,
+            //            typeof(Sandbox).FullName).Unwrap();
+            //return box;
+        }
+
+        public void Execute(string assemblyName, string typeName, string methodName, params object[] parameters)
+        {
+            var assembly = Assembly.Load(assemblyName);
+
+            Type type = assembly.GetType(typeName);
+            if (type == null)
+                return;
+
+            var instance = Activator.CreateInstance(type);
+
+            MethodInfo method = type.GetMethod(methodName);
+            method.Invoke(instance, parameters);
+        }
+
+        public void Dispose()
+        {
+            if (_domain != null)
+            {
+                AppDomain.Unload(_domain);
+
+                _domain = null;
+            }
+        }
+
+        //自定义对象租用周期
+        [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.Infrastructure)]
+        public override Object InitializeLifetimeService()
+        {
+            //将对象的租用周期改变为无限
+            return null;
+
+            //ILease lease = (ILease)base.InitializeLifetimeService();
+            //if (lease.CurrentState == LeaseState.Initial)
+            //{
+            //    lease.InitialLeaseTime = TimeSpan.FromMinutes(1);
+            //    lease.SponsorshipTimeout = TimeSpan.FromMinutes(2);
+            //    lease.RenewOnCallTime = TimeSpan.FromSeconds(2);
+            //}
+            //return lease;
+        }
+    }
+}
--- Windows.TaskSchedule/Utility/ScheduleFactory.cs
+++ Windows.TaskSchedule/Utility/ScheduleFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -7,71 +8,83 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using Quartz;
 using Windows.TaskSchedule.Extends;
-using System.Collections.Concurrent;
-using System.Threading;
 
 namespace Windows.TaskSchedule.Utility
 {
     public class ScheduleFactory : DefaultLogger
     {
-        static readonly string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs", "Jobs.config");
-        static XDocument doc = XDocument.Load(configPath);
-        public readonly static string ServerName = doc.Element("Jobs").Attribute("serverName").Value;
-        public readonly static string Description = doc.Element("Jobs").Attribute("description").Value;
-        public readonly static string DisplayName = doc.Element("Jobs").Attribute("displayName").Value;
-        static readonly object lockObj = new object();
-        static List<JobObject> _jobs = new List<JobObject>();
+        private static readonly string ConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs", "Jobs.config");
+        private static readonly XDocument Doc = XDocument.Load(ConfigPath);
+        public static readonly string ServerName = Doc.Element("Jobs").Attribute("serverName").Value;
+        public static readonly string Description = Doc.Element("Jobs").Attribute("description").Value;
+        public static readonly string DisplayName = Doc.Element("Jobs").Attribute("displayName").Value;
+
+        private static List<JobObject> _jobs = new List<JobObject>();
+
         public void Start()
         {
-            Logger.Debug("服务开始启动...");
+            Logger.Debug("服务开始启动.");
+
             _jobs = GetJobs();
-            new Task(parm =>
-            {
-                var jobs = parm as List<JobObject>;
-                BatchProcess(jobs);
-            }, _jobs).Start();
-            Logger.Debug(string.Format("共找到【{0}】个任务.", _jobs.Count));
-            Logger.Debug(string.Format("当前服务运行目录:【{0}】.", AppDomain.CurrentDomain.BaseDirectory));
+            BatchProcess(_jobs);
+
+            Logger.Debug("共找到【{0}】个任务.", _jobs.Count);
+            Logger.Debug("当前服务运行目录:【{0}】.", AppDomain.CurrentDomain.BaseDirectory);
             Logger.Debug("服务启动成功.");
         }
 
+        public void Stop()
+        {
+            foreach (var job in _jobs)
+            {
+                if (job.RunInSandbox && job.Sandbox != null)
+                {
+                    job.Sandbox.Dispose();
+                }
+            }
 
+            Logger.Debug("服务停止.");
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// 启动轮询任务
+        /// </summary>
+        /// <param name="jobs"></param>
         private void BatchProcess(List<JobObject> jobs)
         {
             foreach (var job in jobs)
             {
-                Task jobTask = new Task(obj =>
+                Task jobTask = new Task(() =>
                 {
                     while (true)
                     {
                         if (!job.Running)
                         {
                             job.Running = true;
-                            var jobParm = obj as JobObject;
+
                             RunJob(job);
-                            jobParm.Running = false;
+
+                            job.Running = false;
                         }
                         System.Threading.Thread.Sleep(100);
                     }
-                }, job);
+                });
+
                 jobTask.Start();
             }
         }
 
-        public void Stop()
-        {
-            Logger.Debug("服务停止.");
-        }
 
-        #region Private Method
         /// <summary>
-        /// 获取配置文件中所有的任务
+        /// 解析配置文件
         /// </summary>
         /// <returns></returns>
         private List<JobObject> GetJobs()
         {
             List<JobObject> result = new List<JobObject>();
-            var jobs = doc.Element("Jobs").Elements("Job");
+            var jobs = Doc.Element("Jobs").Elements("Job");
             foreach (var p in jobs)
             {
                 JobObject job = new JobObject();
@@ -84,7 +97,32 @@ namespace Windows.TaskSchedule.Utility
                     job.JobType = JobTypeEnum.Assembly;
                     string assembly = p.Attribute("type").Value.Split(',')[1];
                     string className = p.Attribute("type").Value.Split(',')[0];
-                    job.Instance = Assembly.Load(assembly).CreateInstance(className) as IJob;
+
+                    string runInSandbox = "false";
+                    if (p.Attributes().Any(o => o.Name.ToString() == "runInSandbox"))
+                    {
+                        runInSandbox = p.Attribute("runInSandbox").Value;
+                    }
+
+                    if (runInSandbox.ToLower() == "true")
+                    {
+                        Random r = new Random();
+                        var name = p.Attribute("name").Value + r.Next(1000);
+
+                        //创建sandbox
+                        job.Sandbox = Sandbox.Create(name);
+                        job.AssemblyName = assembly;
+                        job.TypeName = className;
+
+                        job.RunInSandbox = true;
+                    }
+                    else
+                    {
+                        var targetAssembly = Assembly.Load(assembly);
+                        job.Instance = targetAssembly.CreateInstance(className) as IJob;
+
+                        job.RunInSandbox = false;
+                    }
                 }
                 if (p.Attributes().Any(o => o.Name.ToString() == "ExpireSecond"))
                 {
@@ -114,7 +152,7 @@ namespace Windows.TaskSchedule.Utility
         /// <summary>
         /// 执行任务
         /// </summary>
-        /// <param name="job">要执行的任务</param>
+        /// <param name="job">任务信息</param>
         private void RunJob(JobObject job)
         {
             try
@@ -127,20 +165,29 @@ namespace Windows.TaskSchedule.Utility
                         switch (job.JobType)
                         {
                             case JobTypeEnum.Assembly:
-                                job.Instance.Init();
-                                job.Instance.Excute();
+                                if (job.RunInSandbox)
+                                {
+                                    job.Sandbox.Execute(job.AssemblyName, job.TypeName, "Init", null);
+                                    job.Sandbox.Execute(job.AssemblyName, job.TypeName, "Excute", null);
+                                }
+                                else
+                                {
+                                    job.Instance.Init();
+                                    job.Instance.Excute();
+                                }
+
                                 break;
                             case JobTypeEnum.Exe:
-                                using (var process = new System.Diagnostics.Process())
+                                using (var process = new Process())
                                 {
                                     bool hasValue = job.ExpireSecond.HasValue;
                                     if (string.IsNullOrWhiteSpace(job.Arguments))
                                     {
-                                        process.StartInfo = new System.Diagnostics.ProcessStartInfo(job.ExePath);
+                                        process.StartInfo = new ProcessStartInfo(job.ExePath);
                                     }
                                     else
                                     {
-                                        process.StartInfo = new System.Diagnostics.ProcessStartInfo(job.ExePath, job.Arguments);
+                                        process.StartInfo = new ProcessStartInfo(job.ExePath, job.Arguments);
                                     }
                                     process.Start();
                                     if (hasValue) //如果设置了最长运行时间，到达时间时，自动中止进程
@@ -148,7 +195,7 @@ namespace Windows.TaskSchedule.Utility
                                         bool result = process.WaitForExit(job.ExpireSecond.Value * 1000);
                                         if (!result)
                                         {
-                                            Logger.Info(string.Format("任务【{0}】因长时间：{1}秒未返回运行状态，程序已自动将其Kill.", job.Name, job.ExpireSecond));
+                                            Logger.Info("任务【{0}】因长时间：{1}秒未返回运行状态，程序已自动将其Kill.", job.Name, job.ExpireSecond);
                                             process.Kill();
                                         }
                                     }
@@ -172,12 +219,23 @@ namespace Windows.TaskSchedule.Utility
                 {
                     if (job.JobType == JobTypeEnum.Assembly)
                     {
-                        job.Instance.OnError(ex);
+                        if (job.RunInSandbox)
+                        {
+                            job.Sandbox.Execute(job.AssemblyName, job.TypeName, "OnError", ex);
+                        }
+                        else
+                        {
+                            job.Instance.OnError(ex);
+                        }
                     }
                 }
-                catch { }
+                catch
+                {
+                    // ignored
+                }
             }
         }
+
         #endregion
 
     }
